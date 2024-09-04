174273dd35b910b953544ea250d7de734fad2d27
Yulike Dwi Nurcahyani <yulikecahya@gmail.com>
2024-09-04 17:32:31 +0700

    biodata Ike

--- BiodataMahasiswa.java
+++ BiodataMahasiswa.java
@@ -1,6 +1,11 @@
 public class BiodataMahasiswa {
 
     public static void main(String[] args) {
+        
         System.out.println("Biodata Mahasiswa");
+        System.out.println("Yulike Dwi Nurcahyani");
+        System.out.println("244107020146");
+        System.out.println("Blitar, 05 Juli 2005");
+
     }
 }
