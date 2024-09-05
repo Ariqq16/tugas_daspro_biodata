327a183af8867d50af3ff06a6b23e7044b7bc648
Ariqq16 <ariqbaihaqi011@gmail.com>
2024-09-05 14:05:37 +0700

    biodata ariq

--- BiodataMahasiswa.java
+++ BiodataMahasiswa.java
@@ -2,5 +2,11 @@ public class BiodataMahasiswa {
 
     public static void main(String[] args) {
         System.out.println("Biodata Mahasiswa");
+        System.out.println("Mohammad Ariq Baihaqi");
+        System.out.println("2441070201");
+        System.out.println("Malang, 01 Desember 2005");
+
+
+
     }
 }
